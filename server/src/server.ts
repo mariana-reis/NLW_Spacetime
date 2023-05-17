067e48a5aafc88a1773d5863c8913ee279657e59
mariana-reis <mariana.reis@al.resilia.com.br>
2023-05-17 19:48:41 -0300

    :recycle: refactor: update server ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,18 @@
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
 import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
+app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log(' HTTP server listening on port  http://localhost:3333')
+    console.log('HTTP server listening on port  http://localhost:3333')
   })
