62d71904f4c24066c16268c8a8416d833513fe60
mariana-reis <mariana.reis@al.resilia.com.br>
2023-06-01 15:40:34 -0300

    :sparkles: feat: adding jwt and authRoutes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import 'dotenv/config'
 import fastify from 'fastify'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
@@ -7,6 +10,12 @@ const app = fastify()
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
