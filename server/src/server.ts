f3053feeaaa9c3c3feabfa18d7ddec90860c0a2b
mariana-reis <mariana.reis@al.resilia.com.br>
2023-06-07 10:29:31 -0300

    :sparkles: feat: creating upload routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,18 @@ import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import 'dotenv/config'
 import fastify from 'fastify'
+import { resolve } from 'path'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(require('@fastify/multipart'))
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -17,10 +25,12 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('HTTP server listening on port  http://localhost:3333')
